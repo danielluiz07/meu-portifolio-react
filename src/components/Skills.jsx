472e30be7e905808ca055b04206c2556ad1899a7
Daniel Luiz <dnaieluiz1607@gmail.com>
2026-01-23 12:51:23 -0300

    Add Skills

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { motion } from 'framer-motion';
 
 const skills = [
-  "Java", "Python", "PyAutoGui", "Express.js", "Prisma", "JavaScript", "React", "Node.js", "TailwindCss", "Node.js", "MySQL", "MongoDB", "BootStrap", "Git", "Figma", "GitHub", "Render"
+  "Java", "Python", "JavaScript", "TypeScript", "Node.js", "Express.js", "React", "Prisma", "TailwindCss", "BootStrap", "PyAutoGui", "MySQL", "Postgres" , "SQLite", "MongoDB",  "Git", "GitHub", "Figma",  "Render"
 ];
 
 export default function Skills() {
