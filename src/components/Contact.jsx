859ff34d8990426f2c783a354f81d05d274cfad9
Daniel Luiz <dnaieluiz1607@gmail.com>
2026-02-07 16:26:46 -0300

    add tooltip of contacts

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Github, Linkedin, Mail } from 'lucide-react';
 import Reveal from './Reveal';
+import { Tooltip } from "./ui/tooltip";
 
 export default function Contact({ t }) {
   return (
@@ -11,15 +12,23 @@ export default function Contact({ t }) {
           <p className="text-gray-600 dark:text-gray-400 mb-8 text-lg">{t.contact.text}</p>
           
           <div className="flex justify-center gap-6">
-            <a href="https://github.com/danielluiz07" target="_blank" rel="noreferrer" className="p-4 bg-white dark:bg-gray-800 rounded-full shadow-lg hover:text-blue-500 hover:scale-110 transition border border-gray-100 dark:border-gray-700">
-              <Github size={24} />
-            </a>
-            <a href="https://www.linkedin.com/in/daniel-luiz1607/" target="_blank" rel="noreferrer" className="p-4 bg-white dark:bg-gray-800 rounded-full shadow-lg hover:text-blue-600 hover:scale-110 transition border border-gray-100 dark:border-gray-700">
-              <Linkedin size={24} />
-            </a>
-            <a href="mailto:danielluiz1607@gmail.com" className="p-4 bg-white dark:bg-gray-800 rounded-full shadow-lg hover:text-red-500 hover:scale-110 transition border border-gray-100 dark:border-gray-700">
-              <Mail size={24} />
-            </a>
+            <Tooltip showArrow content="Acesse meu GitHub">
+              <a href="https://github.com/danielluiz07" target="_blank" rel="noreferrer" className="p-4 bg-white dark:bg-gray-800 rounded-full shadow-lg hover:text-blue-500 hover:scale-110 transition border border-gray-100 dark:border-gray-700">
+                <Github size={24} />
+              </a>
+            </Tooltip>
+            
+            <Tooltip showArrow content="Acesse meu Linkedin">
+              <a href="https://www.linkedin.com/in/daniel-luiz1607/" target="_blank" rel="noreferrer" className="p-4 bg-white dark:bg-gray-800 rounded-full shadow-lg hover:text-blue-600 hover:scale-110 transition border border-gray-100 dark:border-gray-700">
+                <Linkedin size={24} />
+              </a>
+            </Tooltip>
+
+            <Tooltip showArrow content="Fale comigo por e-mail">
+              <a href="mailto:danielluiz1607@gmail.com" className="p-4 bg-white dark:bg-gray-800 rounded-full shadow-lg hover:text-red-500 hover:scale-110 transition border border-gray-100 dark:border-gray-700">
+                <Mail size={24} />
+              </a>
+            </Tooltip>
           </div>
         </div>
       </Reveal>
